034cc8eba2d0ee91a10b3db540a358daddc47939
asoderlind <axesode@gmail.com>
2023-03-18 12:00:07 +0900

    first commit

---
+++ configs/null-ls.lua
@@ -0,0 +1,40 @@
+local null_ls = require "null-ls"
+
+local formatting = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+
+local sources = {
+  formatting.prettier,
+  formatting.stylua,
+
+  -- Python stuff
+  formatting.isort,
+  formatting.black,
+  lint.pylint,
+
+  -- Meta formatter
+  formatting.prettierd.with {
+    filetypes = { "html", "yaml", "javascript", "typescript" },
+  },
+}
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        vim.lsp.buf.format { bufnr = bufnr }
+      end,
+    })
+  end
+end
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = on_attach,
+}
