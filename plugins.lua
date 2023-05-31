4f2ca06861aab25f15094b58869445a3a72507a0
asoderlind <axesode@gmail.com>
2023-05-31 17:36:45 +0900

    update github copilot plugin

--- plugins.lua
+++ plugins.lua
@@ -48,13 +48,8 @@ local plugins = {
 
   {
     "zbirenbaum/copilot.lua",
-    -- Lazy load when event occurs. Events are triggered
-    -- as mentioned in:
-    -- https://vi.stackexchange.com/a/4495/20389
+    cmd = "Copilot",
     event = "InsertEnter",
-    -- You can also have it load at immediately at
-    -- startup by commenting above and uncommenting below:
-    -- lazy = false
     opts = overrides.copilot,
   },
 
